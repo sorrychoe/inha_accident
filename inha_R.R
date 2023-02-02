bbec0c3858061ebcaab0e91348f5c0cc5e2a9de8
sorrychoe <cjssoote@gmail.com>
2023-02-03 00:31:54 +0900

    last analysis

--- inha_R.R
+++ inha_R.R
@@ -1,4 +1,4 @@
-library(tidyverse) 
+library(tidyverse)
 library(plotly)
 library(tidytext)
 library(tidylo)
@@ -16,12 +16,8 @@ inha %>%
 inha %>% 
   group_by(군집) %>% 
   tally() %>% 
-  arrange(desc(n)) %>% 
-  ggplot(aes(x = reorder(군집,n), y = n, fill = 군집))+
-  geom_col()+
-  coord_flip()+
-  xlab("")+
-  ylab("")
+  arrange(desc(n)) %>% gt() %>% tab_header('군집 별 기사 빈도') %>% 
+  cols_label(군집 = "군집", n = '빈도')
 
 inha %>% 
   filter(군집 == '인하대 사건 조사보도') %>% 
@@ -31,13 +27,14 @@ inha %>%
   ggplot(aes(x = "", y = n, fill = 언론사))+
   geom_bar(width = 1, stat = 'identity', color = "white")+
   theme(axis.text.y = element_blank(),axis.ticks = element_blank(), legend.text = element_text(size = 9))+
-  geom_text(aes(label = n),
+  geom_text(aes(label = paste(round(n/sum(n)*100, 2),"%")),
             position = position_stack(vjust = 0.5),
             check_overlap = TRUE,
             color = 'white')+
   coord_polar('y', start = 0)+
   xlab("")+
   ylab("")
+
 
 inha %>% 
   filter(군집 == '인하대 사건 그 이후') %>% 
@@ -47,7 +44,7 @@ inha %>%
   ggplot(aes(x = "", y = n, fill = 언론사))+
   geom_bar(width = 1, stat = 'identity', color = "white")+
   theme(axis.text.y = element_blank(),axis.ticks = element_blank(), legend.text = element_text(size = 9))+
-  geom_text(aes(label = n),
+  geom_text(aes(label = paste(round(n/sum(n)*100, 2),"%")),
             position = position_stack(vjust = 0.5),
             check_overlap = TRUE,
             color = 'white')+
@@ -60,8 +57,14 @@ inha %>%
   group_by(언론사) %>% 
   tally() %>% 
   arrange(desc(n)) %>%
-  ggplot(aes(x = reorder(언론사, desc(n)), y = n, fill = 언론사))+
-  geom_col()+
+  ggplot(aes(x = "", y = n, fill = 언론사))+
+  geom_bar(width = 1, stat = 'identity', color = "white")+
+  theme(axis.text.y = element_blank(),axis.ticks = element_blank(), legend.text = element_text(size = 9))+
+  geom_text(aes(label = paste(round(n/sum(n)*100, 2),"%")),
+            position = position_stack(vjust = 0.5),
+            check_overlap = TRUE,
+            color = 'white')+
+  coord_polar('y', start = 0)+
   xlab("")+
   ylab("")
 
@@ -72,29 +75,170 @@ inha %>%
   rowid_to_column() %>%
   unnest_tokens(
     input = "키워드",
-    output = "단어"
-  ) -> inha.words
+    output = "단어") -> inha.words
 
 inha.words %>% 
   group_by(언론사) %>% 
   filter(nchar(단어) >= 2) %>% 
-  count(`단어`, sort = TRUE) %>% 
+  count(`단어`, sort = TRUE) %>%
   slice_max(n, n = 10) %>%
-  ggplot(aes(x = n, y = reorder(단어,n), fill = 언론사)) +
+  ggplot(aes(x = n, y = reorder(단어, n), fill = 언론사)) +
   geom_col(show.legend = F) +
-  facet_wrap(~ 언론사, scales = 'free')
+  facet_wrap(~ 언론사, scales = 'free') + 
+  scale_y_reordered()+
+  xlab("")+
+  ylab("")
 
 inha.words %>% 
   group_by(언론사) %>% 
   filter(nchar(단어) >= 2) %>% 
   count(`단어`, sort = TRUE) %>% 
   bind_log_odds(set = 언론사, feature = 단어, n = n) %>% 
-  arrange(-log_odds_weighted) -> log.odd.df
+  arrange(-log_odds_weighted)-> log.odd.df
+
+log.odd.df$log_odds_weighted <- round(log.odd.df$log_odds_weighted, 3)
 
 log.odd.df %>% 
   slice_max(log_odds_weighted, n = 10) %>%
   ggplot(aes(x = log_odds_weighted, y = reorder(단어,log_odds_weighted), fill = 언론사)) +
   geom_col(show.legend = F) +
   facet_wrap(~ 언론사, scales = 'free') + 
+  scale_y_reordered()+
   xlab("")+
   ylab("")
+
+inha.words %>% 
+  group_by(언론사) %>% 
+  filter(nchar(단어) >= 2) %>% 
+  count(`단어`, sort = TRUE) -> inha.text
+
+texts <- split(inha.text, inha.text$언론사)
+
+top15 <-function(x){
+  x <- x %>% head(20)
+  return(x)
+}
+
+num = 1
+for (k in texts){
+  k <- top15(k)
+  if (num == 1){
+    words = k
+    num = 2
+  }else{
+  words <- cbind(words, k)
+  }
+}
+
+varnames <- c('언론사', '단어', 'n')
+
+names(words)[1:ncol(words)]<- paste0(varnames,"_",1:30)
+
+words %>% 
+  select(!starts_with('언론사')) %>% 
+  gt() %>% 
+  tab_header('언론사 별 단어 빈도') %>% 
+  tab_spanner(label = "경향신문",
+              columns = 1:2) %>% 
+  tab_spanner(label = "국민일보", 
+              columns = 3:4)%>% 
+  tab_spanner(label = "동아일보", 
+              columns = 5:6)%>%
+  tab_spanner(label = "문화일보", 
+              columns = 7:8)%>% 
+  tab_spanner(label = "서울신문", 
+              columns = 9:10)%>%
+  tab_spanner(label = "세계일보", 
+              columns = 11:12)%>% 
+  tab_spanner(label = "조선일보", 
+              columns = 13:14)%>%
+  tab_spanner(label = "중앙일보", 
+              columns = 15:16)%>%
+  tab_spanner(label = "한겨례", 
+              columns = 17:18)%>%
+  tab_spanner(label = "한국일보", 
+              columns = 19:20)%>% 
+  cols_label(
+    단어_2 = "단어",
+    n_3 = "빈도",
+    단어_5 = "단어",
+    n_6 = "빈도",
+    단어_8 = "단어",
+    n_9 = "빈도",
+    단어_11 = "단어",
+    n_12 = "빈도",
+    단어_14 = "단어",
+    n_15 = "빈도",
+    단어_17 = "단어",
+    n_18 = "빈도",
+    단어_20 = "단어",
+    n_21 = "빈도",
+    단어_23 = "단어",
+    n_24 = "빈도",
+    단어_26 = "단어",
+    n_27 = "빈도",
+    단어_29 = "단어",
+    n_30 = "빈도")
+
+texts.odd <- split(log.odd.df, log.odd.df$언론사)
+
+num = 1
+for (k in texts.odd){
+  k <- top15(k)
+  if (num == 1){
+    words.odd = k
+    num = 2
+  }else{
+    words.odd <- cbind(words.odd, k)
+  }
+}
+
+varnames.odd <- c('언론사', '단어', '빈도', '가중로그승산비')
+
+names(words.odd)[1:ncol(words.odd)]<- paste0(varnames.odd,"_",1:40)
+
+words.odd %>% 
+  select(!starts_with('언론사') & !starts_with('빈도')) %>% 
+  gt() %>% 
+  tab_header('언론사 별 단어 상대 빈도') %>% 
+  tab_spanner(label = "경향신문",
+              columns = 1:2) %>% 
+  tab_spanner(label = "국민일보", 
+              columns = 3:4)%>% 
+  tab_spanner(label = "동아일보", 
+              columns = 5:6)%>%
+  tab_spanner(label = "문화일보", 
+              columns = 7:8)%>% 
+  tab_spanner(label = "서울신문", 
+              columns = 9:10)%>%
+  tab_spanner(label = "세계일보", 
+              columns = 11:12)%>% 
+  tab_spanner(label = "조선일보", 
+              columns = 13:14)%>%
+  tab_spanner(label = "중앙일보", 
+              columns = 15:16)%>%
+  tab_spanner(label = "한겨례", 
+              columns = 17:18)%>%
+  tab_spanner(label = "한국일보", 
+              columns = 19:20)%>% 
+  cols_label(
+    단어_2 = "단어",
+    가중로그승산비_4 = "odd",
+    단어_6 = "단어",
+    가중로그승산비_8 = "odd",
+    단어_10 = "단어",
+    가중로그승산비_12 = "odd",
+    단어_14 = "단어",
+    가중로그승산비_16 = "odd",
+    단어_18 = "단어",
+    가중로그승산비_20 = "odd",
+    단어_22 = "단어",
+    가중로그승산비_24 = "odd",
+    단어_26 = "단어",
+    가중로그승산비_28 = "odd",
+    단어_30 = "단어",
+    가중로그승산비_32 = "odd",
+    단어_34 = "단어",
+    가중로그승산비_36 = "odd",
+    단어_38 = "단어",
+    가중로그승산비_40 = "odd")
